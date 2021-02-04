deb4af9f088c063d6ef216c067df007bf4b7ab72
Nikolay Kalashnikov <kalash-job@yandex.ru>
2021-02-04 09:07:43 +0300

    Add migration with pivot table label_task and define m2m relationship between Task and Label

--- app/Models/Label.php
+++ app/Models/Label.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Label extends Model
 {
     use HasFactory;
+
+    public function tasks()
+    {
+        return $this->belongsToMany(Task::class)->withTimestamps();
+    }
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    public function labels()
+    {
+        return $this->belongsToMany(Label::class)->withTimestamps();
+    }
 }
