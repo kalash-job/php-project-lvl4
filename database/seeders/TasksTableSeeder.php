ddedccc6d04f5233815fd25a749b42091314cc76
Nikolay Kalashnikov <kalash-job@yandex.ru>
2021-02-08 22:46:17 +0300

    Add TasksTableSeeder to project

---
+++ database/seeders/TasksTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class TasksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory(10)->create();
+        $tasks = [
+            'тестирование',
+            'разработка',
+            'деплой',
+        ];
+        $id = 1;
+        foreach ($tasks as $taskValue) {
+            $worker = User::find($id);
+            $id++;
+            $creator = User::find($id);
+            $id++;
+            $task = $worker->tasksAssignedToMe()->make(['name' => $taskValue, 'status_id' => 1]);
+            $task->creator()->associate($creator);
+            $task->save();
+        }
+    }
+}
